24c81dcabc51017427d245e5d37e46be40a7f349
Андрей Крапивин <without.a.body@gmail.com>
2024-08-20 14:32:23 +0300

    Refactor

--- src/main/java/telran/stream/StreamTasks.java
+++ src/main/java/telran/stream/StreamTasks.java
@@ -1,6 +1,7 @@
 package telran.stream;
 
 import java.util.Random;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class StreamTasks {
@@ -8,13 +9,16 @@ public class StreamTasks {
         // only one pipeline for getting new array of int's
         // with all numbers from a given array but with different order
         // each method call returns new array in some random order
+        
+        // return IntStream.range(0, arr.length).map(i -> {
+        //     int randomIndex = new Random().nextInt(arr.length - i) + i;
+        //     int temp = arr[randomIndex];
+        //     arr[randomIndex] = arr[i];
+        //     return temp;
+        // }).toArray();
 
-        return IntStream.range(0, arr.length).map(i -> {
-            int randomIndex = new Random().nextInt(arr.length - i) + i;
-            int temp = arr[randomIndex];
-            arr[randomIndex] = arr[i];
-            return temp;
-        }).toArray();
+        return new Random().ints(0, arr.length).distinct().limit(arr.length)
+                .map(i -> arr[i]).toArray();
 
     }
 }
